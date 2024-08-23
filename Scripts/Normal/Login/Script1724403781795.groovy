ac05ab23832c47ccf77ce1dd09804507888e7a7c
askherr <sony.afiif@gmail.com>
2024-08-23 17:08:22 +0700

    update login

--- Scripts/Normal/Login/Script1724403781795.groovy
+++ Scripts/Normal/Login/Script1724403781795.groovy
@@ -17,7 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('https://warehouse.diset.my.id/admin/login')
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(GlobalVariable.url)
 
 WebUI.maximizeWindow()
 
